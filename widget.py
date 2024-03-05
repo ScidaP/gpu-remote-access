6b45023952ce2db85dd8396836e570ab059c24c5
Patricio <patricioscida@gmail.com>
2024-03-05 17:55:41 -0300

    Fixeando errores inicio de sesion

--- widget.py
+++ widget.py
@@ -1,6 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys
-
+import conexiondb as db
 from PySide6.QtWidgets import QApplication, QWidget
 
 # Important:
@@ -9,16 +9,23 @@ from PySide6.QtWidgets import QApplication, QWidget
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_Widget
 
+def inicio_sesion(widget):
+    bbdd = db.db("ejemplo", "admin", "admin")
+    bbdd.conectar()
+    user = widget.ui.text_usuario.toPlainText()
+    passw = widget.ui.text_pass.text()
+    if bbdd.coincide(user, passw):
+        widget.ui.label_mensaje.setText("Iniciando sesion...")
+    widget.ui.label_mensaje.setStyleSheet("color: red;")
+    widget.ui.label_mensaje.setText("Error: Usuario o contraseña incorrectos")
+
+
 class Widget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
-        self.ui.BotonEnviar1.clicked.connect(self.cambiar_texto_label)
-
-    def cambiar_texto_label(self):
-        self.ui.label_2.setText("jeje")
-
+        self.ui.BotonEnviar1.clicked.connect(lambda: inicio_sesion(self))
 
 
 if __name__ == "__main__":
