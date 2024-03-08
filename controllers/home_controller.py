7f735d499eb4b2e7c77433c5d87ddd9936bab32c
Patricio <patricioscida@gmail.com>
2024-03-08 16:58:49 -0300

    Se abre dialog para cargar conexiones nuevas

--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,5 +1,7 @@
 # This Python file uses the following encoding: utf-8
 from requests import get
+from dialog_conexion import dialog_conexion
+from PySide6.QtCore import Qt
 
 class homeController:
     def __init__(self, Dialog):
@@ -7,8 +9,12 @@ class homeController:
         Dialog.ui.l_ip_propia.setText("Tu IP: %s" % obtener_ip())
 
     def crear_conexiones(self, Dialog): # la dejo declarada y la defino luego
-        pass
+        Dialog.ui.b_nueva_conexion.clicked.connect(abrir_dialog_conexion)
 
 def obtener_ip():
     ip = get('https://api.ipify.org').content.decode('utf8')
     return ip
+
+def abrir_dialog_conexion():
+    dialog = dialog_conexion()
+    dialog.exec_()
